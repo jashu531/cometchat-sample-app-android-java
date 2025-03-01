a1843b6bd69620e72c1ea04c73cf207fbf1d5cb8
jashu531 <jashmakwana31@gmail.com>
2025-03-01 09:30:41 +0530

    add files

--- app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
+++ app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
@@ -2,10 +2,9 @@ package com.cometchat.javasampleapp;
 
 public class AppConstants {
 
-    public static final String APP_ID = "XXXXXXXXXXXXXXXXX";
+    public static final String APP_ID = "2710590c35ffd153";
 
-    public static final String AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_KEY = "2f624ff962484c4075f231fc30f6ae463a59f3fc";
 
-    public static final String REGION = "XX";
-
+    public static final String REGION = "in";
 }
